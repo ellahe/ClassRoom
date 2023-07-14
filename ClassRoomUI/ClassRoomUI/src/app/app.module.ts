b0b216a4f49db8cec503df1b74705fdff87010c1
ellahe <ellaheaziziyan@gmail.com>
2023-07-14 12:47:18 +0430

    add material

--- ClassRoomUI/ClassRoomUI/src/app/app.module.ts
+++ ClassRoomUI/ClassRoomUI/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ClerkComponent } from './clerks/clerk/clerk.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ClerkComponent } from './clerks/clerk/clerk.component';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
