fa66dd2c0e7fc21f60bc3920f9451e933d9f9c10
ellahe <ellaheaziziyan@gmail.com>
2023-07-14 12:23:45 +0430

    resolve dependencies

--- ClassRoomUI/ClassRoomUI/src/app/app-routing.module.ts
+++ ClassRoomUI/ClassRoomUI/src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CustomerListComponent } from './customer-list/customer-list.component';
-import { EditCustomerComponent } from './edit-customer/edit-customer.component';
-import { AddCustomerComponent } from './add-customer/add-customer.component';
+
 const routes: Routes = [
         {path: '', pathMatch: 'full', redirectTo: 'contacts'},
-	 {path: 'customers' , component: CustomerListComponent},
-	 {path: 'editCustomer/:email' , component: EditCustomerComponent},
-	 {path: 'addCustomer' , component: AddCustomerComponent}
 
 ];
 
--- ClassRoomUI/ClassRoomUI/src/app/app.module.ts
+++ ClassRoomUI/ClassRoomUI/src/app/app.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AddCustomerComponent } from './add-customer/add-customer.component';
-import { EditCustomerComponent } from './edit-customer/edit-customer.component';
-import { CustomerListComponent } from './customer-list/customer-list.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CustomerService } from './services/cstomer-service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
@@ -14,9 +10,6 @@ import { ActivatedRoute } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
-    AddCustomerComponent,
-    EditCustomerComponent,
-    CustomerListComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,7 +18,7 @@ import { ActivatedRoute } from '@angular/router';
     FormsModule,
     ReactiveFormsModule,
   ],
-  providers: [CustomerService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
