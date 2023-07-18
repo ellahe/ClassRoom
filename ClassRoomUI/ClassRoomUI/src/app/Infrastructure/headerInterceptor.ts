4184669759dd2a0570f7c7eb6b072624ac63b445
ellahe <ellaheaziziyan@gmail.com>
2023-07-18 12:24:34 +0330

    update tp v14

--- ClassRoomUI/ClassRoomUI/src/app/Infrastructure/headerInterceptor.ts
+++ ClassRoomUI/ClassRoomUI/src/app/Infrastructure/headerInterceptor.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core'  
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';  
 import { Observable } from 'rxjs/Observable';  
-import 'rxjs/add/operator/do';  
   
 @Injectable()  
 export class HeaderInterceptor implements HttpInterceptor {  
