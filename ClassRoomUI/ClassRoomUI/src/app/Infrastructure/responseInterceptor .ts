16eaf14da5d42c357778c1132d09995090413698
ellahe <ellaheaziziyan@gmail.com>
2023-07-18 12:11:27 +0330

    upgdare to 14

--- ClassRoomUI/ClassRoomUI/src/app/Infrastructure/responseInterceptor .ts
+++ ClassRoomUI/ClassRoomUI/src/app/Infrastructure/responseInterceptor .ts
@@ -1,31 +1,31 @@
-import { Injectable } from '@angular/core'  
-import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';  
-import { Observable } from 'rxjs/Rx';
+// import { Injectable } from '@angular/core'  
+// import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';  
+// import { Observable } from 'rxjs/Rx';
 
-@Injectable()  
-export class ResponseInterceptor implements HttpInterceptor {  
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {  
+// @Injectable()  
+// export class ResponseInterceptor implements HttpInterceptor {  
+//     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {  
   
-       console.log("Before sending data")  
-        console.log(req);  
-        return next.handle(req)  
-            .retry(3)  
-            .map(resp => {  
-                if (resp instanceof HttpResponse) {  
-                   console.log('Response is ::');  
-                    console.log(resp.body)  
-                }  
-                return resp;  
-            }).catch(err => {  
-                console.log(err);  
-                if (err instanceof HttpResponse)  
- {  
-                    console.log(err.status);  
-                    console.log(err.body);  
-                }  
+//        console.log("Before sending data")  
+//         console.log(req);  
+//         return next.handle(req)  
+//             .retry(3)  
+//             .map(resp => {  
+//                 if (resp instanceof HttpResponse) {  
+//                    console.log('Response is ::');  
+//                     console.log(resp.body)  
+//                 }  
+//                 return resp;  
+//             }).catch(err => {  
+//                 console.log(err);  
+//                 if (err instanceof HttpResponse)  
+//  {  
+//                     console.log(err.status);  
+//                     console.log(err.body);  
+//                 }  
   
-                return Observable.of(err);  
-            });  
+//                 return Observable.of(err);  
+//             });  
   
-    }  
-}  
+//     }  
+// }  
